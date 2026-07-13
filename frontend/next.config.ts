3d6bd9f7cb756b56a0013074433563008c29f06e
oday <odaysaad2003@gmail.com>
2026-07-14 00:12:16 +0300

    edit next and install zod validation

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,10 @@
-import type { NextConfig } from "next";
+import {loadEnvConfig} from "@next/env";
+import type {NextConfig} from "next";
+
+import {getPublicEnv} from "./src/lib/env/public-env";
+
+loadEnvConfig(process.cwd());
+getPublicEnv();
 
 const nextConfig: NextConfig = {};
 
