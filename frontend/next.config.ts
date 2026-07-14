97bc1c6de8e9180f7b6ee56ede47fb445a4f67f2
oday <odaysaad2003@gmail.com>
2026-07-14 22:53:58 +0300

    instal dependensis

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,16 @@ import {getPublicEnv} from "./src/lib/env/public-env";
 loadEnvConfig(process.cwd());
 getPublicEnv();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                pathname: "/**",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
