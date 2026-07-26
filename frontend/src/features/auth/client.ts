bcf23088400aed0da668157b962c3012a049ca74
oday <odaysaad2003@gmail.com>
2026-07-26 15:33:09 +0300

    helper to use the file request api

---
+++ frontend/src/features/auth/client.ts
@@ -0,0 +1,9 @@
+import "client-only";
+
+export {authorizedApiRequest} from "./api/authorized-api-client";
+
+export type {AuthorizedRequestInit} from "./api/authorized-api-client";
+
+export {authUserSchema} from "./schemas/auth.schema";
+
+export type {AuthUser} from "./schemas/auth.schema";
